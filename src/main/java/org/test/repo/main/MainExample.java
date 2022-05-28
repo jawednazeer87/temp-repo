6a63638541a726f6a1470f83b917f39a230c4d15
jawednazeer87 <jawednazeer87@gmail.com>
2022-05-28 22:59:58 +0800

    First commit for new repo

---
+++ src/main/java/org/test/repo/main/MainExample.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package org.test.repo.main;
+
+/**
+ * @author jawednazeer
+ *
+ */
+public class MainExample {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
